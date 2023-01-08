b2e76bf076adcd45d349a918bc46d8d684617d17
Ria Cordero <riarcordero@gmail.com>
2023-01-08 10:24:37 +0800

    Changed fonts to Poppins

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import './index.css'
+import '@fontsource/poppins';
 import router from './router'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- src/pages/main/Home.jsx
+++ src/pages/main/Home.jsx
@@ -1,28 +1,31 @@
-import { Image, Center, Flex, Stack, Title, TextInput, Button, PasswordInput } from '@mantine/core'
+import { Image, Center, Flex, Stack, Title, TextInput, Button, PasswordInput, MantineProvider } from '@mantine/core'
 import { useState } from 'react'
 
-export default function() {
+export default function () {
     return (
-        <main>
-            <Center>
-                <Flex
-                    direction={{ base: 'column', sm: 'row' }}
-                    gap={{ base: 'sm', sm: 'lg' }}
-                    justify={{ sm: 'center' }}>
-                    <Image
-                        justify="center"
-                        width={250}
-                        src="./assets/water.png"
-                        alt="water art from icons8" />
-                    <Title>Welcome to Aqualert</Title>
-                    <TextInput placeholder="username" />
-                    <PasswordInput withAsterisk placeholder="password" />
-                    <Stack>
-                        <Button fullWidth="true" radius="xl" size="md">Login</Button>
-                        <Button fullWidth="true" variant="outline" radius="xl" size="md">Register</Button>
-                    </Stack>
-                </Flex>
-            </Center>
-        </main>
+        <MantineProvider
+            theme={{
+                fontFamily: 'Poppins, sans-serif',
+                headings: { fontFamily: 'Poppins, sans-serif' }
+            }}>
+            <Flex
+                direction={{ base: 'column', sm: 'row' }}
+                gap={{ base: 'sm', sm: 'lg' }}
+                justify={{ sm: 'center' }}
+                align="center">
+                <Image
+                    justify="center"
+                    width={250}
+                    src="./assets/water.png"
+                    alt="water art from icons8" />
+                <Title>Welcome to Aqualert</Title>
+                <TextInput placeholder="username" />
+                <PasswordInput withAsterisk placeholder="password" />
+                <Stack>
+                    <Button fullWidth="true" radius="xl" size="md">Login</Button>
+                    <Button fullWidth="true" variant="outline" radius="xl" size="md">Register</Button>
+                </Stack>
+            </Flex>
+        </MantineProvider>
     )
 }
