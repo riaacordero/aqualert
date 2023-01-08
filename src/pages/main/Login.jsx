c13c7a837a0cb87a024ba5d1486ea37d25138078
Ria Cordero <riarcordero@gmail.com>
2023-01-08 11:24:18 +0800

    Fixed center alignment

--- src/pages/main/Login.jsx
+++ src/pages/main/Login.jsx
@@ -8,26 +8,29 @@ export default function () {
                 fontFamily: 'Poppins, sans-serif',
                 headings: { fontFamily: 'Poppins, sans-serif' }
             }}>
-            <Flex
-                direction='column'
-                gap={{ base: 'sm', sm: 'lg' }}
-                justify= 'center'
-                align='stretch'
-                m={100}>
-                <Image
-                    justify="center"
-                    width={250}
-                    sx={{alignSelf: 'center' }}
-                    src="./assets/water.png"
-                    alt="water art from icons8" />
-                <Title>Welcome to Aqualert!</Title>
-                <TextInput placeholder="username"/>
-                <PasswordInput placeholder="password" />
-                <Stack>
-                    <Button fullWidth="true" radius="xl" size="md">Login</Button>
-                    <Button fullWidth="true" variant="outline" radius="xl" size="md">Register</Button>
-                </Stack>
-            </Flex>
+            <Container my="auto" size={400} h="100%">
+                <Flex
+                    direction='column'
+                    gap='sm'
+                    justify='center'
+                    align='stretch'
+                    w='100%'
+                    h='100%'
+                    sx={{ textAlign: 'center' }}>
+                    <Image
+                        width={250}
+                        sx={{ alignSelf: 'center' }}
+                        src="./assets/water.png"
+                        alt="water art from icons8" />
+                    <Title>Welcome to Aqualert!</Title>
+                    <TextInput placeholder="username" />
+                    <PasswordInput placeholder="password" />
+                    <Stack>
+                        <Button fullWidth="true" radius="xl" size="md">Login</Button>
+                        <Button fullWidth="true" variant="outline" radius="xl" size="md">Register</Button>
+                    </Stack>
+                </Flex>
+            </Container>
         </MantineProvider>
     )
 }
