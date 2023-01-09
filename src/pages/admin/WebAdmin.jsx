d00940bbc87b0790054b0cb56f666d279f15783d
Angelie <angeliebadar1@gmail.com>
2023-01-09 14:10:48 +0800

    Added changes in WebAdmin(w/o maps)

--- src/pages/admin/WebAdmin.jsx
+++ src/pages/admin/WebAdmin.jsx
@@ -1,4 +1,4 @@
-import { Divider, Space, Flex, Stack, Title, TextInput, Button, Paper, Text, Container, MantineProvider, Group, Table, ScrollArea} from '@mantine/core'
+import { Divider, Space, Flex, Stack, Title, TextInput, Button, Paper, Text, Container, MantineProvider, Group, Table, ScrollArea, Select} from '@mantine/core'
 import { IconSearch } from '@tabler/icons'
 import { useState } from 'react'
 
@@ -50,7 +50,7 @@ export default function() {
                         <Title order={2}> Welcome to Aqualert! </Title>
                         <Text fz="sm"> Monitor interruptions all over the city through is your admin dashboard. </Text>
                         <Space h= "xs" />
-                        <Group grow >
+                        <Group position="apart" >
                             <TextInput
                                 placeholder="Search for barangay..."
                                 mt= "lg"
@@ -58,7 +58,7 @@ export default function() {
                                 size="md"
                                 
                             />
-                            <Button  mt= "xl"  radius="xl" size="md">Print History</Button>
+                            <Button  mt= "xl"  radius="xl" size="md" px={50} >Print History</Button>
                         </Group>
                         <Space h= "xl" />
                         
@@ -89,17 +89,55 @@ export default function() {
                                     
                         </Paper>  
 
+                        <Space h= "xl" />   
+
                         <Paper radius="xs" pl={10} pr={40} py={5} withBorder >
                             <Text fz="xl" fw={500}>Report Details </Text>
                             <Divider my="xs" size="sm"/>
+
+                            <Stack spacing={0}>
+                               
+                                <Flex direction= 'row' gap= "sm">
+                                    <Flex direction="column">
+                                        <Text fz="xs" fw={500}>Complainant: </Text>
+                                        <Text fz="xs" fw={500}>Billing Account No.: </Text>
+                                        <Text fz="xs" fw={500}>Report Date: </Text>
+                                    </Flex>
+                                    <Flex direction="column">
+                                        <Text fz="xs" >John C. Doe </Text>
+                                        <Text fz="xs" >0012000345 </Text>
+                                        <Text fz="xs" >10/22/2022 | 8:12 AM </Text>
+                                    </Flex>
+                                </Flex>
                             
-                            <Flex direction="row" gap='sm'  wrap="wrap">
+                                <Space h= "xl" />
+                                <Text fz="xs" fw={500}>Message: </Text>
+                                <Text fz="xs" >Hello maam/sir... wala nsay 2big diri sa mag brgy LUBOGAN, TORIL, Davao City sukad kagabii. Naay nabangga nga truck unahan saamong kanto, bcg mao toy hinungdan ngano naputol nasd ang supply sa 2big.. unta ma SOLUSYONAN dayon. salamat </Text>
                                 
-                            </Flex>
-                                    
+                                
+                                
+                            </Stack>
+                            
                         </Paper>  
                         
+                        <Space h= "xl" />
 
+                        <Group position='apart'>
+                            <Select
+                                label="Set Status: "
+                                placeholder="Pick one"
+                                data={[
+                                    { value: 'interrupted', label: 'Interrupted' },
+                                    { value: 'ongoingRepair', label: 'Ongoing Repair' },
+                                    { value: 'scheduledInterruption', label: 'Scheduled Interruption' },
+                                    { value: 'interruptionReported', label: 'Interruption Reported' },
+                                    { value: 'good', label: 'Good' }
+                                ]}
+                            />
+                            <Button  mt= "xl"  radius="xl" size="md" px={60}>Send</Button>
+
+                        </Group>
+                        
                         
                         
                     </Stack>
