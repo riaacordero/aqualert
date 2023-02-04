c8a1138513e61ee432ef40de7c19168926ac79bd
Ria Cordero <riarcordero@gmail.com>
2023-02-04 15:04:27 +0800

    Minor color adjustments

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 export const STATUS_TYPES = [
-    { value: 'interruptionReported', label: 'Interruption Reported', color: '#F08C00' },
+    { value: 'interruptionReported', label: 'Interruption Reported', color: '#5C5F66' },
     { value: 'interrupted', label: 'Interrupted', color: '#F03E3E' },
-    { value: 'ongoingRepair', label: 'Ongoing Repair', color: '#FCC419' },
-    { value: 'scheduledInterruption', label: 'Scheduled Interruption', color: '#228BE6' },
+    { value: 'ongoingRepair', label: 'Ongoing Repair', color: '#228BE6' },
+    { value: 'scheduledInterruption', label: 'Scheduled Interruption', color: '#F08C00' },
     { value: 'good', label: 'Good', color: '#40C057' }
 ];
