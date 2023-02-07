bab6e8ea1b7e961f11281bbf497df5cd06aed814
Ria Cordero <riarcordero@gmail.com>
2023-02-07 09:57:09 +0800

    Added ScrollArea for tables

--- src/pages/admin/WebAdmin.jsx
+++ src/pages/admin/WebAdmin.jsx
@@ -1,10 +1,10 @@
-import { Divider, Space, Flex, Stack, Title, TextInput, Button, Paper, Text, MantineProvider, Group, Table, ScrollArea, Select, Container } from '@mantine/core'
-import { IconSearch } from '@tabler/icons'
+import { Divider, Space, Flex, Stack, Title, Modal, Button, Paper, Text, ActionIcon, Group, Table, ScrollArea, Select, Container } from '@mantine/core'
+import { IconSearch, IconPower } from '@tabler/icons'
 import { DataTable } from 'mantine-datatable';
 import { MapContainer, Marker, TileLayer, useMap } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-
 import { useEffect, useRef, useState } from 'react'
+import { useAuth } from '../../context'
 import { addDoc, collection, doc, documentId, getDocs, query, QuerySnapshot, setDoc, updateDoc, where } from 'firebase/firestore';
 import { db } from '../../firebase';
 import dayjs from 'dayjs';
@@ -31,6 +31,8 @@ export default function () {
     const [reports, setReports] = useState([]);
     const [reportId, setCurrentReportId] = useState(null);
     const [report, setReport] = useState(null);
+    const [opened, setOpened] = useState(false);
+    const { user, signout } = useAuth();
     const form = useForm({
         initialValues: {
             status: STATUS_TYPES[0].value
@@ -167,124 +169,153 @@ export default function () {
                         <Button  mt= "xl"  radius="xl" size="md" px={50} >Print History</Button>
                     </Group> */}
 
-                    <Space h="xl" />
-                    <DataTable
-                        withBorder
-                        borderRadius="sm"
-                        withColumnBorders
-                        striped
-                        highlightOnHover
+                    <Modal
+                        opened={opened}
+                        zIndex={99999}
+                        onClose={() => setOpened(false)}
+                        title="Are you sure you want to logout?">
+                        <Group>
+                            <Button
+                                onClick={() => {
+                                    signout();
+                                }}
+                                radius="xl"
+                                color='red'
+                                fullWidth>Yes, log me out
+                            </Button>
+                            <Button
+                                onClick={() => {
+                                    setOpened(false)
+                                }
+                                }
+                                radius="xl"
+                                variant="outline" color='red'
+                                fullWidth>No, take me back
+                            </Button>
+                        </Group>
+                    </Modal>
 
-                        records={reports.map(r => ({
-                            id: r.id,
-                            billAccount: r.user?.billingNo ?? 'Unknown',
-                            barangay: r.consumer_data?.barangay?.barangay_name ?? 'Unknown',
-                            street_name: r.consumer_data?.street_name ?? 'Unknown',
-                            previousStatus: STATUS_TYPES[Math.max((r.consumer_data?.status ?? 1) - 1, 0)].label
-                        }))}
-                        // define columns
-                        columns={[
-                            {
-                                accessor: 'billAccount',
-                                title: 'Billing Account No.',
-                                textAlignment: 'left',
-                            },
-                            {
-                                accessor: 'barangay',
-                                title: 'Barangay'
-                            },
-                            {
-                                accessor: 'street_name',
-                                title: 'Street Name'
-                            },
-                            {
-                                accessor: 'previousStatus',
-                                title: 'Previous Status',
-                                // this column has custom cell data rendering
-                                render: ({ previousStatus }) => (
-                                    <Text weight={700} color={STATUS_TYPES.find(s => s.label === previousStatus).color}>
-                                        {previousStatus.toUpperCase()}
-                                    </Text>
-                                ),
+                    <Space h="xl" />
+                    <ScrollArea style={{height: 720}}>
+                        
+                        <DataTable
+                            withBorder
+                            borderRadius="sm"
+                            withColumnBorders
+                            striped
+                            highlightOnHover
+
+                            records={reports.map(r => ({
+                                id: r.id,
+                                billAccount: r.user?.billingNo ?? 'Unknown',
+                                barangay: r.consumer_data?.barangay?.barangay_name ?? 'Unknown',
+                                street_name: r.consumer_data?.street_name ?? 'Unknown',
+                                previousStatus: STATUS_TYPES[Math.max((r.consumer_data?.status ?? 1) - 1, 0)].label
+                            }))}
+                            // define columns
+                            columns={[
+                                {
+                                    accessor: 'billAccount',
+                                    title: 'Billing Account No.',
+                                    textAlignment: 'left',
+                                },
+                                {
+                                    accessor: 'barangay',
+                                    title: 'Barangay'
+                                },
+                                {
+                                    accessor: 'street_name',
+                                    title: 'Street Name'
+                                },
+                                {
+                                    accessor: 'previousStatus',
+                                    title: 'Previous Status',
+                                    // this column has custom cell data rendering
+                                    render: ({ previousStatus }) => (
+                                        <Text weight={700} color={STATUS_TYPES.find(s => s.label === previousStatus).color}>
+                                            {previousStatus.toUpperCase()}
+                                        </Text>
+                                    ),
+                                }
+                            ]}
+                            // execute this callback when a row is clicked
+                            onRowClick={({ id }) =>
+                                setCurrentReportId(id)
                             }
-                        ]}
-                        // execute this callback when a row is clicked
-                        onRowClick={({ id }) =>
-                            setCurrentReportId(id)
-                        }
-                    />
+                        />
+                    </ScrollArea>
                 </Stack>
                 <Stack sx={{ flex: 1, flexGrow: 1 }} h="100%">
-                <Paper radius="xs" pl={10} pr={40} py={5} mb="xl" withBorder >
-                                <Flex direction="row" gap='sm' wrap="wrap">
-                                    <Text fz="md" fw={500}>Selected Barangay: </Text>
-                                    <Text fz={16} >{report?.consumer_data?.barangay?.barangay_name}</Text>
+                    <Paper radius="xs" pl={10} pr={40} py={5} mb="xl" withBorder >
+                        <Flex direction="row" gap='sm' wrap="wrap">
+                            <Text fz="md" fw={500}>Selected Barangay: </Text>
+                            <Text fz={16} >{report?.consumer_data?.barangay?.barangay_name}</Text>
+                        </Flex>
+                    </Paper>
+
+                    <MapContainer
+                        center={[7.03285, 125.49727]}
+                        zoom={20}
+                        scrollWheelZoom={false}
+                        style={{ height: '400px' }}>
+                        <TileLayer
+                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+
+                        <Panner
+                            coords={report?.consumer_data ?
+                                [report?.consumer_data?.latitude, report?.consumer_data?.longitude] : null} />
+
+                        {report?.consumer_data &&
+                            <Marker position={[report?.consumer_data.latitude, report?.consumer_data.longitude]} />}
+                    </MapContainer>
+
+                    <Space h="xl" />
+
+                    <Paper radius="xs" pl={10} pr={40} py={5} withBorder >
+                        <Text fz="xl" fw={500}>Report Details </Text>
+                        <Divider my="xs" size="xs" color="dark.7" />
+
+                        <Stack spacing={0}>
+                            <Flex direction='row' gap="sm">
+                                <Flex direction="column">
+                                    <Text fz="xs" fw={500}>Complainant: </Text>
+                                    <Text fz="xs" fw={500}>Billing Account No.: </Text>
+                                    <Text fz="xs" fw={500}>Report Date: </Text>
                                 </Flex>
-                            </Paper>
-
-                            <MapContainer
-                                center={[7.03285, 125.49727]}
-                                zoom={20}
-                                scrollWheelZoom={false}
-                                style={{ height: '400px' }}>
-                                <TileLayer
-                                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                                    url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
-                                />
-
-                                <Panner
-                                    coords={report?.consumer_data ?
-                                        [report?.consumer_data?.latitude, report?.consumer_data?.longitude] : null} />
-
-                                {report?.consumer_data &&
-                                    <Marker position={[report?.consumer_data.latitude, report?.consumer_data.longitude]} />}
-                            </MapContainer>
+                                <Flex direction="column">
+                                    <Text fz="xs">{report?.user?.firstName} {report?.user?.middleName} {report?.user?.lastName}</Text>
+                                    <Text fz="xs">{report?.user?.billingNo}</Text>
+                                    <Text fz="xs">
+                                        {dayjs(report?.interruptDate?.toDate()).format('MM/DD/YYYY | h:mm A')}
+                                    </Text>
+                                </Flex>
+                            </Flex>
 
                             <Space h="xl" />
+                            <Text fz="xs" fw={500}>Message: </Text>
+                            <Text fz="xs" >
+                                {report?.complaintMsg}
+                            </Text>
+                        </Stack>
+                    </Paper>
 
-                            <Paper radius="xs" pl={10} pr={40} py={5} withBorder >
-                                <Text fz="xl" fw={500}>Report Details </Text>
-                                <Divider my="xs" size="xs" color="dark.7" />
-
-                                <Stack spacing={0}>
-                                    <Flex direction='row' gap="sm">
-                                        <Flex direction="column">
-                                            <Text fz="xs" fw={500}>Complainant: </Text>
-                                            <Text fz="xs" fw={500}>Billing Account No.: </Text>
-                                            <Text fz="xs" fw={500}>Report Date: </Text>
-                                        </Flex>
-                                        <Flex direction="column">
-                                            <Text fz="xs">{report?.user?.firstName} {report?.user?.middleName} {report?.user?.lastName}</Text>
-                                            <Text fz="xs">{report?.user?.billingNo}</Text>
-                                            <Text fz="xs">
-                                                {dayjs(report?.interruptDate?.toDate()).format('MM/DD/YYYY | h:mm A')}
-                                            </Text>
-                                        </Flex>
-                                    </Flex>
-
-                                    <Space h="xl" />
-                                    <Text fz="xs" fw={500}>Message: </Text>
-                                    <Text fz="xs" >
-                                        {report?.complaintMsg}
-                                    </Text>
-                                </Stack>
-                            </Paper>
-
-                            <form onSubmit={form.onSubmit(({ status }) => {
-                                if (!report || !report?.consumer_data || !report?.user) return;
-                                const newStatus = STATUS_TYPES.findIndex(s => s.value === status) ?? report?.consumer_data?.status - 1 ?? 0;
-                                changeStatus(report, newStatus + 1);
-                            })}>
-                                <Group position='apart'>
-                                    <Select
-                                        label="Set Status: "
-                                        placeholder="Pick one"
-                                        data={STATUS_TYPES}
-                                        {...form.getInputProps('status')}
-                                    />
-                                    <Button type="submit" mt="xl" radius="xl" size="md" px={60}>Send</Button>
-                                </Group>
-                            </form>
+                    <form onSubmit={form.onSubmit(({ status }) => {
+                        if (!report || !report?.consumer_data || !report?.user) return;
+                        const newStatus = STATUS_TYPES.findIndex(s => s.value === status) ?? report?.consumer_data?.status - 1 ?? 0;
+                        changeStatus(report, newStatus + 1);
+                    })}>
+                        <Stack>
+                            <Select
+                                label="Select New Status: "
+                                placeholder="Pick one"
+                                data={STATUS_TYPES}
+                                {...form.getInputProps('status')}
+                            />
+                            <Button type="submit" radius="xl" size="lg">Set Status</Button>
+                        </Stack>
+                    </form>
                 </Stack>
             </Group>
         </Container>
