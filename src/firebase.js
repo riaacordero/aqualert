a1243aedc9412b1e9ee5d610d9ee682fc0a67525
riaacordero <riarcordero@gmail.com>
2023-03-21 20:39:48 +0800

    Updated Firebase dependencies

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,19 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth} from 'firebase/auth';
-import { documentId, getFirestore } from 'firebase/firestore';
+import { getFirestore } from 'firebase/firestore';
 import { getDatabase } from 'firebase/database';
 
-
+// NOTE: Do not directly put values here to avoid accidents upon developing on a production data.
+// Please put the values into the .env or .env.development file instead.
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY = "AIzaSyBeUupxsmi3MUSSgx2YB2VRoH7VZdpeYBI" ,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN = "aqualert-f4ac4.firebaseapp.com",
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID = "aqualert-f4ac4",
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET = "aqualert-f4ac4.appspot.com",
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID = "89793290410",
-  appId: import.meta.env.VITE_FIREBASE_APP_ID = "1:89793290410:web:39f332c15a60ecd1e9f0d3",
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID = "G-E7PGXTRVN0"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
